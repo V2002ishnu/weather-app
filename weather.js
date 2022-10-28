1f52625780d9704cb65db8901e26b565db138cae
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2022-10-29 01:16:26 +0530

    added files

---
+++ weather.js
@@ -0,0 +1,38 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+    
+const getWeather = async(city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    console.log(data)
+    return showWeather(data)
+}
+
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
+    weather.innerHTML = `
+        
+        <div class = "data">
+            <h4>Temp: ${data.main.temp} ℃</h4>
+            <h4>Feels Like: ${data.main.feels_like} ℃</h4>
+            <h4>Humidity: ${data.main.humidity}</h4>
+
+            <h4>${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
